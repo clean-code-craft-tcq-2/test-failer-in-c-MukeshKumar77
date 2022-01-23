814b5846237f3ba818ab4ecba64ce4e070164d0e
Mukesh <90908610+MukeshKumar77@users.noreply.github.com>
2022-01-23 13:48:09 +0530

    Update alerter_test_code.h

--- alerter_test_code.h
+++ alerter_test_code.h
@@ -4,3 +4,19 @@
 int FaliureCountForTemperatureBreach;
 
 int networkAlertStub(float celcius);
+int networkAlertStub(float celcius) {
+    int tempThreshold;
+	printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+    // Return 200 for ok
+    // Return 500 for not-ok
+    if(celcius >= 0.0 && celcius <= 200.0)
+    {
+    	tempThreshold = OK_TEMPERATURE;
+    }
+    else
+    {
+    	tempThreshold = NOT_OK_TEMPERATURE;
+    	FaliureCountForTemperatureBreach++;
+    }
+    return tempThreshold;
+}
